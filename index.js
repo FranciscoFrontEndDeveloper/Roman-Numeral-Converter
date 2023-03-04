ac1f8f4dde940013272e67fcc7dd7ee1857e3e92
FRANCISCO <pachonetworking@gmail.com>
2023-03-03 21:10:53 -0500

    base de inicio

---
+++ index.js
@@ -0,0 +1,21 @@
+function convertToRoman(num) {
+  let NumberA = Array.from(String(num), Number);
+  switch (NumberA.length) {
+    case 1:
+      console.log("la cantidad de numero es: " + NumberA.length);
+      break;
+    case 2:
+      console.log("la cantidad de numero es: " + NumberA.length);
+      break;
+    case 3:
+      console.log("la cantidad de numero es: " + NumberA.length);
+      break;
+    case 4:
+      console.log("la cantidad de numero es: " + NumberA.length);
+      break;
+    default:
+      break;
+  }
+}
+
+convertToRoman(3);
