1f3bb7d47b184d461f898a8b245674d225a433fb
FRANCISCO <pachonetworking@gmail.com>
2023-03-04 20:10:59 -0500

    del uno al tres romano listo

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function convertToRoman(num) {
   let NumberA = Array.from(String(num), Number);
+  let unit = "";
+  function romanUnits123() {
+    if (NumberA[0] >= 1 && NumberA[0] <= 3) {
+      unit = "I";
+      for (let index = 1; index < NumberA[0]; index++) {
+        unit += "I";
+      }
+      console.log(unit);
+      return unit;
+    }
+  }
+
   switch (NumberA.length) {
     case 1:
-      console.log("la cantidad de numero es: " + NumberA.length);
+      romanUnits123();
+      // romanUnits45678();
       break;
     case 2:
-      console.log("la cantidad de numero es: " + NumberA.length);
       break;
     case 3:
       console.log("la cantidad de numero es: " + NumberA.length);
+      console.log(NumberA[0], NumberA[1], NumberA[2]);
       break;
     case 4:
       console.log("la cantidad de numero es: " + NumberA.length);
+      console.log(NumberA[0], NumberA[1], NumberA[2], NumberA[3]);
       break;
     default:
       break;
