11d1e9d1b1dee26b8e5c6e5ffd387a48a19eda0f
FRANCISCO <pachonetworking@gmail.com>
2023-03-20 15:45:46 -0500

    firts function without parameter

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function convertToRoman(num) {
   let NumberA = Array.from(String(num), Number);
+  console.log(NumberA)
   let unit = "";
   function romanUnits123() {
     if (NumberA[0] >= 1 && NumberA[0] <= 3) {
       unit = "I";
+      for (let index = 1; index < NumberA[0]; index++) {
+        unit += "I";
+      }
+      console.log(unit);
+      return unit;
+    } else if (NumberA[0] == 4) {
+      unit = "V";
+      for (let index = 1; index < NumberA[0]; index++) {
+        "I" + unit;
+      }
+      console.log("I" + unit);
+      return unit;
+    } else if (NumberA[0] >= 5 && NumberA[0] <= 8) {
+      unit = "V";
       for (let index = 1; index < NumberA[0]; index++) {
         unit += "I";
       }
@@ -17,19 +32,20 @@ function convertToRoman(num) {
       romanUnits123();
       // romanUnits45678();
       break;
-    case 2:
-      break;
-    case 3:
-      console.log("la cantidad de numero es: " + NumberA.length);
-      console.log(NumberA[0], NumberA[1], NumberA[2]);
-      break;
-    case 4:
-      console.log("la cantidad de numero es: " + NumberA.length);
-      console.log(NumberA[0], NumberA[1], NumberA[2], NumberA[3]);
-      break;
+    // case 2:
+    //   break;
+    // case 3:
+    //   console.log("la cantidad de numero es: " + NumberA.length);
+    //   console.log(NumberA[0], NumberA[1], NumberA[2]);
+    //   break;
+    // case 4:
+    //   console.log("la cantidad de numero es: " + NumberA.length);
+    //   console.log(NumberA[0], NumberA[1], NumberA[2], NumberA[3]);
+    //   break;
     default:
+      console.log('sin numeros')
       break;
   }
 }
 
-convertToRoman(3);
+convertToRoman(5);
