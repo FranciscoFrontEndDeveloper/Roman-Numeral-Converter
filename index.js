e93a1886de7393d1966fa23985425f6fe2a8df58
FRANCISCO <pachonetworking@gmail.com>
2023-04-13 00:49:33 -0500

    tens finished

--- index.js
+++ index.js
@@ -32,31 +32,6 @@ function convertToRoman(num) {
     }
   }
 
-  // function romanUnits459(numberTest) {
-  //   if (numberTest == 4) {
-  //     unit = "IV";
-  //     console.log(unit);
-  //     return unit;
-  //   } else if (numberTest == 5) {
-  //     unit = "V";
-  //     console.log(unit);
-  //     return unit;
-  //   } else if (numberTest == 9) {
-  //     unit = "IX";
-  //     console.log(unit);
-  //     return unit;
-  //   }
-  // }
-
-  // function romanUnits678(numberTest) {
-  //   let residuoPrueba = numberTest % 5;
-  //   if (numberTest >= 6 && numberTest <= 8) {
-  //     unit = "V" + romanUnits123(residuoPrueba);
-  //     console.log(unit);
-  //     return unit;
-  //   }
-  // }
-
   function romanTens123(numberTest) {
     if (numberTest >= 1 && numberTest <= 3) {
       unit = "X";
@@ -65,17 +40,30 @@ function convertToRoman(num) {
       }
       console.log(unit + romanUnits123(NumberA[1]));
       return unit;
-    } else if (numberTest == 0 || undefined) {
-      unit = " ";
+    } else if (numberTest == 4) {
+      unit += "XL";
+      console.log(unit + romanUnits123(NumberA[1]));
+    } else if (numberTest == 5) {
+      unit += "L";
+      console.log(unit + romanUnits123(NumberA[1]));
+    } else if (numberTest >= 6 && numberTest <= 8) {
+      unit = "L";
+      for (let index = 5; index < numberTest; index++) {
+        unit += "X";
+      }
+      console.log(unit + romanUnits123(NumberA[1]));
       return unit;
+    } else if (numberTest == 9) {
+      unit += "XC";
+      console.log(unit + romanUnits123(NumberA[1]));
     }
+    console.log(unit);
+    return unit;
   }
 
   switch (NumberA.length) {
     case 1:
       romanUnits123(NumberA[0]);
-      // romanUnits459(NumberA[0]);
-      // romanUnits678(NumberA[0]);
       break;
     case 2:
       romanTens123(NumberA[0]);
@@ -86,4 +74,4 @@ function convertToRoman(num) {
   }
 }
 
-convertToRoman(1);
+convertToRoman(90);
